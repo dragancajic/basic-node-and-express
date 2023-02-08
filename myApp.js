023a49f4d74f35b785a83e6f7fdd37b83fccdf75
Dragan Cajic <cajic_dragan@yahoo.com>
2023-02-08 13:15:09 +0100

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -3,14 +3,15 @@ let app = express();
 
 console.log("Hello World");
 
+app.use('/public' , express.static(__dirname + '/public'));
+console.log("path to public static assets:", __dirname + '/public');
+
 app.get("/", function(req, res) {
 	//res.send("Hello Express");
 	res.sendFile(__dirname + '/views/index.html');
-	console.log("variable __dirname is:", __dirname + '/views/index.html');
+	console.log("variable __dirname is:", __dirname);
+	console.log("path to view in HTML:", __dirname + '/views/index.html');
 });
-
-
-
 
 
 
