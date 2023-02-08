ca83f36129622d8ddb36200863c13ee497bfb8ce
Dragan Cajic <cajic_dragan@yahoo.com>
2023-02-08 23:27:58 +0100

    Serve JSON on a Specific Route
    > The endpoint `/json` should serve the JSON object `{"message": "Hello json"}`.

--- myApp.js
+++ myApp.js
@@ -3,14 +3,20 @@ let app = express();
 
 console.log("Hello World");
 
-app.use('/public' , express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 console.log("path to public static assets:", __dirname + '/public');
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
 	//res.send("Hello Express");
 	res.sendFile(__dirname + '/views/index.html');
 	console.log("variable __dirname is:", __dirname);
 	console.log("path to view in HTML:", __dirname + '/views/index.html');
+});
+
+
+app.get("/json", function (req, res) {
+	data = { "message": "Hello json" };
+	return res.json(data);
 });
 
 
@@ -35,10 +41,4 @@ app.get("/", function(req, res) {
 
 
 
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
